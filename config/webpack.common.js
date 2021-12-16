604e1fb193839975a57bccdca90db91e9bfc1309
atumv <atumv.gm@gmail.com>
2021-12-16 20:57:55 +0700

    Update webpack.common.js

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, '../src/index.tsx'),
+  entry: path.resolve(__dirname, '../src/index.js'),
 
   output: {
     path: path.resolve(__dirname, '../build'),

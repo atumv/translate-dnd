a18d3c36e5ac9556de05fafd0b4ae1e542490126
atumv <atumv.gm@gmail.com>
2021-12-16 23:09:49 +0700

    Fix phrase rendering

--- src/components/Phrase.tsx
+++ src/components/Phrase.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import StyledPhrase from "../styles/StyledPhrase";
 
-const Phrase: React.FC = () => <StyledPhrase />;
+type PhraseProps = {
+  children: React.ReactNode;
+};
+
+const Phrase: React.FC<PhraseProps> = ({children}) => (
+  <StyledPhrase>{children}</StyledPhrase>
+);
 
 export default Phrase;
